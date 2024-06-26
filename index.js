039fad2ffdd8255f200444ed371d511532adffc9
tfchetan <166944165+tfchetan@users.noreply.github.com>
2024-06-26 13:18:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+
+const pass_field = document.querySelector("#password");
+const showBtn = document.querySelector(".show");
+showBtn.addEventListener("click", function () {
+  if (pass_field.type === "password") {
+    pass_field.type = "text";
+    showBtn.textContent = "Hide";
+    showBtn.style.color = "#fff";
+  } else {
+    pass_field.type = "password";
+    showBtn.textContent = "Show";
+    showBtn.style.color = "#fff";
+  }
+});
+
